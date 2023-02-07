d4e20528b5188a675349f1b897ae6942e25d741a
akashgaurav1997 <akashchaudhary720@yahoo.com>
2023-02-07 19:51:02 +0530

    firtstcommit

---
+++ jsproblem/index.js
@@ -0,0 +1,15 @@
+// let name="Akashgaurav";
+// let school="St Francis";
+// let grade=  "A";
+// let rollno= 745;
+// let marks_scored_maths=80;
+// let marks_scored_hindi=80;
+// let marks_score_geography=80;
+
+// console.log("*****__________"+school+"____________*****");
+// console.log("Name:",name);
+// console.log("Grade:",grade);
+// console.log("Roll:",rollno);
+// console.log("Maths:",marks_scored_maths);
+// console.log("Hindi:",marks_scored_hindi);
+// console.log("Geography:",marks_score_geography);
